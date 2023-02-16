08fbcd7764ae0084487be706e19de5bd9c9dbd9c
ItomiI <89792132+ItomiI@users.noreply.github.com>
2023-02-16 19:32:35 -0300

    mejoras
    mejoras

--- portfolio/src/app/servicios/EliminarDato.service.ts
+++ portfolio/src/app/servicios/EliminarDato.service.ts
@@ -9,27 +9,16 @@ import { Dato } from 'src/Interfaces';
 })
 export class EliminarDatoService {
 
-  URL:string = "http://localhost:8080/miapi/datos/editar";
+  URL:string = "http://localhost:8080/miapi/datos/";
+  log:string = "http://localhost:8080/auto/verif";
 
   constructor(private http:HttpClient,private cookies: CookieService) { }
 
-<<<<<<< HEAD
   eliminar(id:number):Observable<boolean>{
     var laurl=this.URL + id.toString();
     return this.http.delete<boolean>(laurl);
-     
-=======
-  eliminar(id:number):Observable<Dato>{
-    let a = this.cookies.get("mitoken");
-    let b= {
-      clave:a,
-      dato:id
-  
-    }
-    return this.http.put<Dato>(this.URL,b);
->>>>>>> parent of c9f1762 (c)
+    
   }
 
-
-
 }
+
